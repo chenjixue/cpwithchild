987a98d38838c4bc06064009ab8681e2e556d174
chenjixue <736538448@qq.com>
2025-02-20 19:23:29 +0800

    支持路径别名

--- bin/start.js
+++ bin/start.js
@@ -2,14 +2,26 @@
 // const program = require("commander");
 // const chalk = require("chalk");
 // const copyFile = require("../lib/copyFile.cjs");
-import {program } from "commander";
-import chalk  from "chalk";
-import copyFile from "../lib/copyFile.cjs"
+import { program } from "commander";
+import chalk from "chalk";
+import copyFile from "../lib/copyFile.cjs";
+// 定义你的命令
 program
-  .argument('<url>', '源文件的 URL')  // 定义一个参数 <url>
+  .argument("<url>", "源文件的 URL") // 定义一个参数 <url>
+  .option("-a, --alias <value>", "传入参数")
   .description("源文件的url") //用来描述命令干啥的
-  .action((pn, cmd) => {
-    copyFile(pn)
+  .action((pn, options) => {
+    let aliasObject = {};
+    if (options.alias) {
+      let aliasArr = options.alias.split(" ");
+      aliasArr.forEach((item) => {
+        if (item) {
+          let [key, value] = item.split("=");
+          aliasObject[key] = value;
+        }
+      });
+    }
+    copyFile(pn,aliasObject);
   });
 program.on("--help", () => {
   console.log(
